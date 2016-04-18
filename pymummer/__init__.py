e0a5aaa56d0d8b464cee4a20e3328ac7a580a331
Martin Hunt <mh12@sanger.ac.uk>
2016-04-18 15:56:28 +0100

    Implement pymummer.__version__

--- pymummer/__init__.py
+++ pymummer/__init__.py
@@ -1,3 +1,11 @@
+from pkg_resources import get_distribution
+
+try:
+    __version__ = get_distribution('pymummer').version
+except:
+    __version__ = 'local'
+
+
 __all__ = [
     'alignment',
     'coords_file',
