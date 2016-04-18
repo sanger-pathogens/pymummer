120f81631f9289787f89f8d0eeb890a4c6c06d2d
Martin Hunt <mh12@sanger.ac.uk>
2016-04-18 15:57:38 +0100

    Version bump 0.7.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if not found_all_progs:
 
 setup(
     name='pymummer',
-    version='0.7.0',
+    version='0.7.1',
     description='Wrapper for MUMmer',
     packages = find_packages(),
     author='Martin Hunt, Nishadi De Silva',
